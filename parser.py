066d6cd57ebf393c9569fdf5598681246c1cd046
Klemen Tušar <techouse@gmail.com>
2022-05-21 14:05:39 +0100

    :wrench: Parse new docs versions

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup
 
-FLASK_VERSIONS = ('2.0.x', '1.1.x', '1.0.x', '0.12.x')
+FLASK_VERSIONS = ('2.1.x', '2.0.x', '1.1.x', '1.0.x', '0.12.x')
 FLASK_DOCS_URL = 'https://flask.palletsprojects.com/en/{version}/'
 EXCLUDED_IDS = {'index-', 'module-'}
 
